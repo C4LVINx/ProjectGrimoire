3c30d7c3a885e59bcd2f3b3e1cc40b7721448689
Homelessman221 <ulrikschaeffer.2002@gmail.com>
2023-05-01 21:26:44 +0200

    Finished relics that can be moved together

--- Assets/Scripts/Ulrik Scripts/RelicRelicAttacher.cs
+++ Assets/Scripts/Ulrik Scripts/RelicRelicAttacher.cs
@@ -6,35 +6,110 @@ using UnityEngine.UIElements;
 public class RelicRelicAttacher : MonoBehaviour
 {
     private DraggableObject draggable;
+ 
 
-    [SerializeField] private float overlapBoxScale = 1;
+    [SerializeField] private float overlapBoxScale = 1f;
+    [HideInInspector] public bool IsAttachable;
 
     private GameObject currentRelic;
+    public GameObject RelicThisAttachedTo;
     [SerializeField] private LayerMask RelicMask;
 
-    [SerializeField] private Transform attachedRelicPos;
+    [SerializeField] public Transform attachedRelicPos;
+    [SerializeField] public Transform attachedRelicLeft;
     private Collider2D detectedRelic;
     private void Start()
     {
         draggable = GetComponent<DraggableObject>();
+      
     }
 
 
     private void Update()
     {
-        if(currentRelic == null)
+        DetectRelic();
+        ChildRelic();
+        RelicDetaching();
+    }
+
+    private void DetectRelic()
+    {
+        if (currentRelic == null)
         {
-            if (Physics2D.OverlapBox(attachedRelicPos.position, new Vector2(overlapBoxScale, overlapBoxScale), 0, RelicMask))
+            if (Physics2D.OverlapBox(attachedRelicPos.position + new Vector3(-0.4f, -0.6f, 0), new Vector2(overlapBoxScale, overlapBoxScale), 0, RelicMask))
             {
-                detectedRelic = Physics2D.OverlapBox(attachedRelicPos.position, new Vector2(overlapBoxScale, overlapBoxScale), 0, RelicMask);
-                if (!detectedRelic.gameObject.GetComponent<DraggableObject>().isHeld && !draggable.isHeld)
+                detectedRelic = Physics2D.OverlapBox(attachedRelicPos.position + new Vector3(-0.4f, -0.6f, 0), new Vector2(overlapBoxScale, overlapBoxScale), 0, RelicMask);
+                if (!detectedRelic.gameObject.GetComponent<DraggableObject>().isHeld && !draggable.isHeld && IsAttachable && detectedRelic.GetComponent<RelicRelicAttacher>().IsAttachable)
                 {
                     currentRelic = detectedRelic.gameObject;
+                    currentRelic.GetComponent<RelicRelicAttacher>().RelicThisAttachedTo = gameObject;
                 }
+
+  
             }
             else
             {
                 detectedRelic = null;
+            }
+        }
+    }
+
+    private void ChildRelic()
+    {
+        if(currentRelic != null)
+        {
+
+        
+
+            if(currentRelic.GetComponent<DraggableObject>().isHeld)
+            {
+                transform.position = currentRelic.GetComponent<RelicRelicAttacher>().attachedRelicLeft.position;
+            }
+            else
+            {
+                currentRelic.transform.position = attachedRelicPos.position;
+            }
+        }
+    }
+
+    private void RelicDetaching()
+    {
+        if(draggable.isHeld)
+        {
+            IsAttachable = true;
+            if(detectedRelic != null)
+            {
+                detectedRelic.GetComponent<RelicRelicAttacher>().IsAttachable = true;
+            }
+        }
+        if(detectedRelic != null)
+        {
+            if (detectedRelic.GetComponent<DraggableObject>().isHeld)
+            {
+                IsAttachable = true;
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(currentRelic != null)
+            {
+                IsAttachable = false;
+            currentRelic.GetComponent<RelicRelicAttacher>().IsAttachable = false;
+                currentRelic.GetComponent<RelicRelicAttacher>().RelicThisAttachedTo = null;
+            currentRelic = null;
+          
+            }
+
+            if(RelicThisAttachedTo != null)
+            {
+                IsAttachable = false;
+                RelicThisAttachedTo.GetComponent<RelicRelicAttacher>().IsAttachable=false;
+                RelicThisAttachedTo.GetComponent<RelicRelicAttacher>().currentRelic = null;
+                RelicThisAttachedTo = null;
             }
         }
     }
@@ -44,7 +119,7 @@ public class RelicRelicAttacher : MonoBehaviour
         //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
 
         //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-        Gizmos.DrawWireCube(attachedRelicPos.position, new Vector2(overlapBoxScale, overlapBoxScale));
+        Gizmos.DrawWireCube(attachedRelicPos.position + new Vector3(-0.4f,-0.8f,0), new Vector2(overlapBoxScale, overlapBoxScale));
         // Gizmos.DrawWireCube(attachedRelicPos.position, new Vector2(scale, scale));
     }
 
