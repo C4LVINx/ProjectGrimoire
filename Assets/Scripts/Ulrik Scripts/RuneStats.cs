d5457ff1ec0a79bd648f87ec914dfcb29a6e286f
Homelessman221 <ulrikschaeffer.2002@gmail.com>
2023-05-01 15:34:24 +0200

    Made system for categorizing runes

---
+++ Assets/Scripts/Ulrik Scripts/RuneStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneStats : MonoBehaviour
+{
+    [SerializeField] private Sprite RuneSprite;
+    public int[] WhatSpell;
+    public int[] WhatRelic;
+    public int[] WhatSlot;
+}
