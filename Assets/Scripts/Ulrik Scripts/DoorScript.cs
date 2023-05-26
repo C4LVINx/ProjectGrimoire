f7b0f56a7abcec4390ffbf8def5a5bfce4745d86
Homelessman221 <ulrikschaeffer.2002@gmail.com>
2023-05-26 21:01:11 +0200

    Added updated text
    Added updated but still placeholder text,  as well as changing the way doors work. They are no longer UI objects, but instead GameObjects

--- Assets/Scripts/Ulrik Scripts/DoorScript.cs
+++ Assets/Scripts/Ulrik Scripts/DoorScript.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class DoorScript : MonoBehaviour
 {
+    [SerializeField]private bool nextRoom = true;
+    [SerializeField] private bool isPhilozopherStone;
+
+    [SerializeField] private GameObject nextRoomGameobjects;
+    [SerializeField] private GameObject nextRoomUIObjects;
+
+    [SerializeField] private GameObject previousRoomGameobjects;
+    [SerializeField] private GameObject previousRoomUIObjects;
     public void NextRoom()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +21,29 @@ public class DoorScript : MonoBehaviour
     public void PreviousRoom()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+
+    private void OnMouseDown()
+    {
+        if(nextRoom)
+        {
+            nextRoomGameobjects.SetActive(true);
+            nextRoomUIObjects.SetActive(true);
+
+            previousRoomGameobjects.SetActive(false);
+            previousRoomUIObjects.SetActive(false);
+        }
+        if(!nextRoom)
+        {
+            previousRoomGameobjects.SetActive(true);
+            previousRoomUIObjects.SetActive(true);
+
+            nextRoomGameobjects.SetActive(false);
+            nextRoomUIObjects.SetActive(false);
+        }
+        if(isPhilozopherStone)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
